fb021e762d6ae13b974d42c703580f0de71a60ee
Amy <amysun.uk@gmail.com>
2024-08-14 11:43:15 +0100

    added routing and page for details

--- Controllers/CatalogueController.cs
+++ Controllers/CatalogueController.cs
@@ -8,17 +8,19 @@ public class CatalogueController : Controller
 {
     private readonly ILogger<CatalogueController> _logger;
 
+    private readonly Bookish.BookishContext _context;
+
     public CatalogueController(ILogger<CatalogueController> logger)
     {
         _logger = logger;
+        _context = new BookishContext();
     }
 
     public IActionResult BookList()
     {
-         var context = new BookishContext();
         BookshelfModel allBooks = new BookshelfModel();
 
-        allBooks.Books = (from book in context.Books select new BookModel()
+        allBooks.Books = (from book in _context.Books select new BookModel()
         {
             Id=book.Id,
             Name=book.Name,
@@ -29,6 +31,33 @@ public class CatalogueController : Controller
         return View(allBooks);
     }
     
+    public IActionResult Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        BookshelfModel foundBooks = new BookshelfModel();
+
+        foundBooks.Books = (from book in _context.Books where book.Id == id select new BookModel()
+        {
+            Id=book.Id,
+            Name=book.Name,
+            Author=book.Author,
+            Genre=book.Genre
+        }).ToList();
+        
+        // book = _context.Books.First(book => book.Id == id);
+
+        if (foundBooks.Books == null)
+        {
+            return NotFound();
+        }
+
+        return View(foundBooks);
+    }
+
     [HttpPost]
     public ActionResult SearchResult()
     {
